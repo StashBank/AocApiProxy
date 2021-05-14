acab002166c82d362d53f0603441b9994c4ed0bd
StashBank <opavlovskyi@gmail.com>
2021-05-14 16:50:21 +0300

    ppp

--- index.ts
+++ index.ts
@@ -5,25 +5,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const targetApiUrl = process.env.TARGET_API_URL;
-const targetWebAppUrl = process.env.TARGET_API_URL;
-
-const proxyServer = proxy.createProxyMiddleware('/', {
-    target: targetApiUrl,
-    changeOrigin: true,
-    secure: false,
-    onProxyReq: (proxyReq, req, res) => {
-        const authorization = req.headers['authorization'];
-        console.log('AUTH_TOKEN %s', authorization);
-        proxyReq.setHeader('Cookie', `intrepidUser=${authorization}`);
-    },
-    onProxyRes: (proxyRes, req, res) => {
-        const origin = req.headers['origin'] || req.headers['referer'];
-        if (origin) {
-            proxyRes.headers['access-control-allow-origin'] =  origin;
-        }
-    }
-})
+const targetWebAppUrl = process.env.TARGET_WEBAPP_URL;
 
 const webAppProxyServer = proxy.createProxyMiddleware('/', {
     target: targetWebAppUrl,
@@ -31,24 +13,10 @@ const webAppProxyServer = proxy.createProxyMiddleware('/', {
     secure: false
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 const app = express();
 
-app.get('/', (req, res) => res.send(`
-    <div style="height: 100%;width:100%;display:flex;justify-content:center;align-items:center">
-        <div>
-            <p>AOC API Proxy server</p>
-            <p>Target is: ${process.env.TARGET_API_URL}</p>
-        </div>
-    </div>
-`));
-
-//app.use('/api/*', proxyServer);
-app.use('*', webAppProxyServer);
-
-app.use('/shop', (req, res) => {
-    res.redirect('https://aoc-op.web.app/shop')
-})
+app.use('/*', webAppProxyServer);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
